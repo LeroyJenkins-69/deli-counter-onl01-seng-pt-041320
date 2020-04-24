69788181fea48a6339546673d597bec5c0d88e34
Kordell Parrish <parrishkordell@gmail.com>
2020-04-24 20:27:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,15 +1,12 @@
 def line
   katz_deli = []
-  while katz_deli.size == nil
+  while katz_deli.length = nil 
   puts "The line is currently empty."
   end
 end
 
 def take_a_number(current,new)
-  current = 
-  new = 
 end 
 
 def now_serving 
-
 end 
